5c22f47c92e243a45cb2355d0336e4f0ad097ed2
烽正 楊 <iefcyang@gmail.com>
2021-10-21 00:45:51 +0800

    2021 10 20 HWK3 Project created.

---
+++ ImageProcessingHWK3/ImageFilter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProcessingHWK3
+{
+    class ImageMask
+    {
+
+    }
+
+    class ImageFilter
+    {
+        // Masks
+
+
+        // LaplacianOfGaussian operator: (second derivative of a Gaussian filter)
+        // (1) smooth (Gaussian filter) => convolve (Laplacian)
+
+        // Basic zero-crossing detector (threshold)
+        // Marr-Hildreth edge detection vs Sobel operator.
+
+
+        // 
+
+
+
+        // Order-statistic filter : max, min, and median 
+
+
+    }
+}
